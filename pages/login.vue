016c0c9b9724241c4607ee9213ad6643bcf51a70
cjiao100 <1971416012@qq.com>
2019-07-24 22:27:33 +0800

    登录注册页面布局基本完成

---
+++ pages/login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="page-login">
+    <article class="login-header">
+      <div class="logo"></div>
+    </article>
+    <section class="login-panel">
+      <div class="banner">
+        <img
+          src="//s0.meituan.net/bs/file/?f=fe-sso-fs:build/page/static/banner/www.jpg"
+          alt="美团网"
+        />
+      </div>
+      <div class="form">
+        <h4 class="tip">{{ error }}</h4>
+        <p><span>账号登录</span></p>
+        <el-input v-model="username" prefix-icon="profile" />
+        <el-input v-model="password" prefix-icon="password" />
+        <div class="foot">
+          <el-checkbox v-model="checked">7天自动登录</el-checkbox>
+          <b>忘记密码？</b>
+        </div>
+        <el-button class="btn-login" type="success" size="mini" @click="login">
+          登录
+        </el-button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'blank',
+  data() {
+    return {
+      error: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/css/login/index.scss';
+</style>
